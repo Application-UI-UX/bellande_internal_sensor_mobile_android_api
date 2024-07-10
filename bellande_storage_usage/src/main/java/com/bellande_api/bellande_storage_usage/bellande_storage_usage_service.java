728658f51802365adc763a0222ff2523681fbadc
RonaldsonBellande <ronaldsonbellande@gmail.com>
2024-07-10 16:33:43 -0400

    latest pushes

--- bellande_storage_usage/src/main/java/com/bellande_api/bellande_storage_usage/bellande_storage_usage_service.java
+++ bellande_storage_usage/src/main/java/com/bellande_api/bellande_storage_usage/bellande_storage_usage_service.java
@@ -48,10 +48,10 @@ public class bellande_storage_usage_service {
             if (response.isSuccessful() && response.body() != null) {
                 return response.body().getCpuUsage();
             } else {
-                throw new RuntimeException("Error getting NETWORK usage: " + response.code() + " - " + response.message());
+                throw new RuntimeException("Error getting STORAGE usage: " + response.code() + " - " + response.message());
             }
         } catch (IOException e) {
-            throw new RuntimeException("Error getting NETWORK usage: " + e.getMessage());
+            throw new RuntimeException("Error getting STORAGE usage: " + e.getMessage());
         }
     }
 
@@ -63,10 +63,10 @@ public class bellande_storage_usage_service {
             if (response.isSuccessful() && response.body() != null) {
                 return response.body().getStatus();
             } else {
-                throw new RuntimeException("Error sending NETWORK usage: " + response.code() + " - " + response.message());
+                throw new RuntimeException("Error sending STORAGE usage: " + response.code() + " - " + response.message());
             }
         } catch (IOException e) {
-            throw new RuntimeException("Error sending NETWORK usage: " + e.getMessage());
+            throw new RuntimeException("Error sending STORAGE usage: " + e.getMessage());
         }
     }
 }
